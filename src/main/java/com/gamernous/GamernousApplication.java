101f35599ac6f56aba6f2bd021f2bc2582f321b1
JULIAN RAFAEL DE SOUZA <julian93bully@gmail.com>
2024-12-07 00:34:25 -0300

    feat(gamernous): initialize project
    Set up basic project structure and dependencies.

---
+++ src/main/java/com/gamernous/GamernousApplication.java
@@ -0,0 +1,13 @@
+package com.gamernous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamernousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamernousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamernous/GamernousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamernous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamernousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
